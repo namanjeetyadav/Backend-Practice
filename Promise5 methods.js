3020d6acc1b38533fe7b3bfd11cc3b4dc41b48f5
namanjeet <namanshuttler@gmail.com>
2024-06-11 17:23:34 +0530

    11.06.24 C

--- Promise5 methods.js
+++ Promise5 methods.js
@@ -26,20 +26,24 @@ let P3 = new Promise((resolve, reject) => {
 })
 
 
-P1.then((value) => {
-    console.log("Promise 1 has been resolved with his " + value)
+P1.then((value, t1) => {
+    console.log("Promise 1 has been resolved with in " + t1 + " time with value " + value)
 })
 
-P2.then((value) => {
-    console.log("Promise 2 has been resolved with his " + value)
+P2.then((value, t2) => {
+    console.log("Promise 2 has been resolved with in " + t2 + " time with value " + value)
 })
 
-P3.then((value) => {
-    console.log("Promise 3 has been resolved with his " + value)
+P3.then((value, t3) => {
+    console.log("Promise 3 has been resolved with in " + t3 + " time with value " + value)
 })
 
-Promise.all([P1, P2, P3]).then((value)=>{
+Promise.all([P1, P2, P3]).then((value) => {
     console.log(value)
-}).catch((error) =>{
+}).catch((error) => {
     console.error(error);
 });
+
+Promise.race([P1, P2, P3]).then((value) => {
+    console.log(value)
+})
