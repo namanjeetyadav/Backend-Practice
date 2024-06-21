45a3081f399832bd7bce49912a022d2d8e800d47
namanjeet <namanshuttler@gmail.com>
2024-06-21 17:01:42 +0530

    21.06.24 A

---
+++ 17_Currency_converter_app.js
@@ -0,0 +1,11 @@
+
+
+const dropdown = document.querySelector(".all-selects select");
+
+
+let optionElement = document.createElement("option");
+optionElement.value = "1"; 
+optionElement.textContent = "2"; 
+
+dropdown.appendChild(optionElement);
+
