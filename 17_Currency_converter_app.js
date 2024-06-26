ae2159dcfb0780376ca137bb7e33d38edb4f568f
namanjeet <namanshuttler@gmail.com>
2024-06-26 17:03:36 +0530

    26.06.24

--- 17_Currency_converter_app.js
+++ 17_Currency_converter_app.js
@@ -1,4 +1,12 @@
+// const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json"
+
 const dropdown = document.querySelectorAll(".all-select select")
+
+const btn = document.querySelector("form button");
+
+const fromCurr = document.querySelector(".from select")
+
+const toCurr = document.querySelector(".to select")
 
 for (let select of dropdown) {
     for (let curr in countryList) {
@@ -14,4 +22,46 @@ for (let select of dropdown) {
 
         select.append(newOption);
     }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
 }
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+
+}
+
+// const URl = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currCode}.json`
+
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+
+}
+
+
+
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();  //Restrict the default behaviour of form to refresh the page while submission.
+    let amount = document.querySelector(".amount input")
+    let amountValue = amount.value;
+    if (amountValue === "" || amountValue < 1) {
+        amountValue = 1;
+        amount.value = "1";
+    }
+    updateExchangeRate();
+
+});
+
+console.log(fromCurr.value, toCurr.value);
+
+
+
