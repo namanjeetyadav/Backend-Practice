374360f1570bf0213cd2d36f2461b56f42d2abb2
namanjeet <namanshuttler@gmail.com>
2024-08-17 00:10:22 +0530

    17.08.24

--- 20_Constructors.js
+++ 20_Constructors.js
@@ -1,9 +1,30 @@
-class RailwayForm {
-    constructor(givename, trainno){
+class railwayForm {
+    constructor(givename, trainno, address) {
         console.log("CONSTRUCTOR CALLED")
+        this.name = givename;
+        this.trainno = trainno;
+        this.address = address;
     }
 
-    submit(){
-        alert("This form is submitted")
+    preview() {
+        alert("Hey, " + this.name + " this is your form details: " + "Train Number: " + this.trainno + " Address: " + this.address)
+
+
     }
+    submit() {
+        alert("Hey, " + this.name + " this form is submitted,  form details: " + "Train Number: " + this.trainno + " Address: " + this.address)
+    }
+
+    cancel() {
+        alert("This from is cancelled")
+    }
+
 }
+
+let namanForm = new railwayForm("Naman", "125680", "Kalyanpur Kanpur")
+let amanForm = new railwayForm("Aman", "125680", "Kalyanpur Kanpur")
+
+namanForm.preview()
+namanForm.submit()
+amanForm.preview()
+amanForm.submit()
