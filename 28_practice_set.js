573356e46fdc6f3fb8689c13d83a36f9297d4816
namanjeet <namanshuttler@gmail.com>
2024-09-10 21:55:19 +0530

    10.09.24

--- 28_practice_set.js
+++ 28_practice_set.js
@@ -1,11 +1,45 @@
-class Complex {
-    constructor(real, imaginary) {
-        this.real = real
-        this.imaginary = imaginary
+// Method to add two complex number 
+
+// class Complex {
+//     constructor(real, imaginary) {
+//         this.real = real
+//         this.imaginary = imaginary
+//     }
+
+//     add(num){
+//         this.real = this.real + num.real
+//         this.imaginary = this.imaginary + num.imaginary
+//     }
+// }
+
+// let a = new Complex(4,2)
+// let b = new Complex(6,2)
+
+// a.add(b)
+
+// console.log(`${a.real} + ${a.imaginary}i`)
+
+
+// Create a class student from a class human, override a method and see changes
+
+class Human {
+    constructor(name) {
+    this.name = name;
+    
     }
 
-    add(num){
-        this.real = this.real + num.real
-        this.imaginary = this.imaginary + num.imaginary
+    breath() {
+        console.log("Human can breath")
+    }
+
+    walk() {
+        console.log("Human can walk")
     }
 }
+
+class Student extends Human {
+    walk() { console.log(`${this.name} - Student can walk fast than human`) }
+}
+
+let o = new Student("Naman")
+o.walk()
